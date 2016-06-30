8e1a1deeca4f4582e42cce0f15e056c4a332d863
Michael Petrov <adm_fil_02@ro66.fss.ru>
2016-06-30 21:21:13 +0500

    new def - 2

--- module1.py
+++ module1.py
@@ -4,6 +4,9 @@
 def first():
 	print('this is first function on module #1')
 
+def second(s):
+	print('it\'s a second module - {}'.format(s))
+
 if __name__ == '__main__':
 	first()
-
+	second('yes')
